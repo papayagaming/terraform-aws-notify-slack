c44a900433e019fea001dc05f2ddd9b6e549bc84
Amitai Getzler <amitaig@papaya.com>
2023-12-04 12:51:32 -0500

    :heavy_plus_sign: Ad

--- functions/notify_slack.py
+++ functions/notify_slack.py
@@ -327,8 +327,6 @@ def get_log_for_alarm(alarm_name):
     start_query_response = logs.start_query(
         logGroupName=LOG_GROUP,
         queryString=filterPattern,
-        startTime=int((datetime.today() - timedelta(minutes=5)).timestamp()),
-        endTime=int(datetime.now().timestamp()),
         limit=1  # Change the limit according to your requirement
     )
     query_id = start_query_response['queryId']
