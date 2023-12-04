5e03a101870af02e3c83b91fa2084f4cbb8e1885
Amitai Getzler <amitaig@papaya.com>
2023-12-04 12:47:36 -0500

    :heavy_plus_sign: Add

--- functions/notify_slack.py
+++ functions/notify_slack.py
@@ -99,16 +99,19 @@ def format_cloudwatch_alarm(message: Dict[str, Any], region: str) -> Dict[str, A
     if LOG_GROUP:
         alarm_reason = get_log_for_alarm(alarm_name)
         print(alarm_reason)
-        relevant_info = json.loads(alarm_reason['results'][0][1]['value'])
-        relevant_info = {
-            'arn': relevant_info['userIdentity']['arn'],
-            'eventTime': relevant_info['eventTime'],
-            'eventName': relevant_info['eventName'],
-            'sourceIPAddress': relevant_info['sourceIPAddress'],
-            'userAgent': relevant_info['userAgent'],
-            'requestParameters': relevant_info['requestParameters'],
-            'awsRegion': relevant_info['awsRegion']
-        }
+        try:
+            alarm_reason = json.loads(alarm_reason['results'][0][1]['value'])
+            relevant_info = {
+                'arn': relevant_info['userIdentity']['arn'],
+                'eventTime': relevant_info['eventTime'],
+                'eventName': relevant_info['eventName'],
+                'sourceIPAddress': relevant_info['sourceIPAddress'],
+                'userAgent': relevant_info['userAgent'],
+                'requestParameters': relevant_info['requestParameters'],
+                'awsRegion': relevant_info['awsRegion']
+            }
+        except Exception as e:
+            print(e)
 
     return {
         # "color": CloudWatchAlarmState[message["NewStateValue"]].value,
