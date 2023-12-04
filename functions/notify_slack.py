2b4bf1a2e5d9006bed618e4de9bacc69f97d1d8f
Amitai Getzler <amitaig@papaya.com>
2023-12-04 13:59:16 -0500

    :heavy_plus_sign: Add

--- functions/notify_slack.py
+++ functions/notify_slack.py
@@ -323,6 +323,7 @@ def format_default(
     return attachments
 
 def get_log_for_alarm(alarm_name, namespace):
+    print("Alarm name: ", alarm_name, "Namespace: ", namespace)
     filterPattern = logs.describe_metric_filters(limit=1,metricName=alarm_name, metricNamespace=namespace)
     print(filterPattern)
     filterPattern = filterPattern['metricFilters'][0]['filterPattern']
