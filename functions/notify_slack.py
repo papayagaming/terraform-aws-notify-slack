b040490a20f239b69e4e621419d6708d0e25b6ae
Amitai Getzler <amitaig@papaya.com>
2023-12-05 11:28:51 -0500

    :heavy_plus_sign: Add

--- functions/notify_slack.py
+++ functions/notify_slack.py
@@ -320,7 +320,7 @@ def get_log_for_alarm(alarm_name, namespace):
     result = logs.filter_log_events(
         limit=100,
         filterPattern=filterPattern.strip(),
-        startTime=int((datetime.today() - timedelta(minutes=5)).timestamp()),
+        startTime=int((datetime.today() - timedelta(minutes=60)).timestamp()),
         # endTime=int(datetime.now().timestamp()),
         logGroupName=LOG_GROUP)
     print(result)
